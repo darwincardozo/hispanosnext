1c26317eb22ebf805d89b920fb603a08d7159d1c
cardozodaa@gmail.comgit config --global user.email cardozodaa@gmail.comgit config --global user.name darwincardozogit config --global user.email cardozodaa@gmail.com <cardozodaa@gmail.com>
2021-11-09 17:23:10 -0400

    Se agrega Maquetacion en Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import '../demo1/dist/assets/css/style.bundle.css'
+import '../demo1/dist/assets/plugins/global/plugins.bundle.css'
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
